6bed1ec91660eaa11a3f4cbf3679df2d6e6c315e
Francisco_261199 <franciscoribeiro2611@gmail.com>
2023-05-26 18:40:43 +0100

    read from filesystem

--- src/RPI/server/routes/videos.js
+++ src/RPI/server/routes/videos.js
@@ -1,15 +1,40 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const token = require('../controllers/auth')
+
 var router = express.Router()
 
+var RECORDING_FOLDER = "~/camera/multimedia/"
 
+router.get('/', (req, res) => {
+    response = token.authenticateToken(req,res)
+    if (response !== "User validated") {
+        console.log(response)
+        return res.status(400).json({ message: response })
+    }
 
-router.get('/', token.authenticateToken, (req, res) => {
-    res.json({
+    fs.readdir(RECORDING_FOLDER, (err, files) => {
+        if (err) {
+          console.error('Error reading folder:', err);
+          return;
+        }
+        var files = []
+        files.forEach((file) => {
+          if (file == nil) {
+            return
+          }
+            const filePath = path.join(RECORDING_FOLDER, file);
+      
+          console.log(filePath);
+        });
+    });
+
+    res.status(200).json({
         "id": "test",
         "url": "test",
-        "dateTime": new Date().toString
+        "dateTime": new Date(),
     })
 });
     
