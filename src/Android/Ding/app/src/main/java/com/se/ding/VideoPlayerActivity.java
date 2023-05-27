57cc7524c771ecae0ebcad95416e8c378e7e23b8
DaniilLystopadskyi <danlys1967@gmail.com>
2023-05-27 21:58:35 +0100

    Improved player visuals

--- src/Android/Ding/app/src/main/java/com/se/ding/VideoPlayerActivity.java
+++ src/Android/Ding/app/src/main/java/com/se/ding/VideoPlayerActivity.java
@@ -2,79 +2,167 @@ package com.se.ding;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
 
-import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.ProgressiveMediaSource;
-import com.google.android.exoplayer2.ui.PlayerView;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.util.Util;
 import com.se.ding.network.Client;
 import com.se.ding.network.Stream;
 
 import org.videolan.libvlc.LibVLC;
 import org.videolan.libvlc.Media;
 import org.videolan.libvlc.MediaPlayer;
+import org.videolan.libvlc.interfaces.IVLCVout;
+import org.videolan.libvlc.util.VLCVideoLayout;
 
 import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-public class VideoPlayerActivity extends AppCompatActivity implements SurfaceHolder.Callback {
-    private SurfaceView surfaceView;
-    private SurfaceHolder surfaceHolder;
-    private LibVLC libVLC;
-    private MediaPlayer mediaPlayer;
+
+public class VideoPlayerActivity extends AppCompatActivity  {
+    private static final boolean USE_TEXTURE_VIEW = false;
+    private static final boolean ENABLE_SUBTITLES = true;
+
+    private VLCVideoLayout mVideoLayout = null;
+
+    private LibVLC mLibVLC = null;
+    private MediaPlayer mMediaPlayer = null;
+    private MediaController controller;
+
+    private Boolean isStreaming = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_video_player);
+        isStreaming = getIntent().getExtras().getBoolean("isStreaming");
 
-        surfaceView = findViewById(R.id.surface_view);
-        surfaceHolder = surfaceView.getHolder();
-        surfaceHolder.addCallback(this);
+        final ArrayList<String> args = new ArrayList<>();
+        args.add("-vvv");
+        mLibVLC = new LibVLC(this, args);
+        mMediaPlayer = new MediaPlayer(mLibVLC);
 
-        libVLC = new LibVLC(this);
-        mediaPlayer = new MediaPlayer(libVLC);
+        mVideoLayout = findViewById(R.id.video_layout);
+
+        View mVideoSurfaceFrame = findViewById(R.id.video_surface_frame);
+        controller = new MediaController(this);
+        controller.setMediaPlayer(playerInterface);
+        controller.setAnchorView(mVideoSurfaceFrame);
+        mVideoSurfaceFrame.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                controller.show(10000);
+            }
+        });
+
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        mediaPlayer.getVLCVout().setVideoSurface(holder.getSurface(), null);
-        mediaPlayer.getVLCVout().attachViews();
+    protected void onDestroy() {
+        super.onDestroy();
+        mMediaPlayer.release();
+        mLibVLC.release();
+        if(isStreaming) {
+            SharedPreferences sharedPref = getSharedPreferences("preferences", Context.MODE_PRIVATE);
+            String accessToken = sharedPref.getString("access_token", null);
+            if (accessToken != null) {
+                Call<Void> call = Client.getService().stopStream("Bearer " + accessToken);
+                call.enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                    }
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                    }
+                });
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mMediaPlayer.attachViews(mVideoLayout, null, ENABLE_SUBTITLES, USE_TEXTURE_VIEW);
 
         Uri uri = Uri.parse(getIntent().getStringExtra("videoUrl"));
-        Media media = new Media(libVLC, uri);
-        mediaPlayer.setMedia(media);
+        Media media = new Media(mLibVLC, uri);
+        mMediaPlayer.setMedia(media);
         media.release();
-
-        mediaPlayer.play();
+        mMediaPlayer.play();
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    protected void onStop() {
+        super.onStop();
+
+        mMediaPlayer.stop();
+        mMediaPlayer.detachViews();
     }
 
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        mediaPlayer.stop();
-        mediaPlayer.release();
-        libVLC.release();
-    }
+    private MediaController.MediaPlayerControl playerInterface = new MediaController.MediaPlayerControl() {
+        public int getBufferPercentage() {
+            return 0;
+        }
+
+        public int getCurrentPosition() {
+            float pos = mMediaPlayer.getPosition();
+            return (int)(pos * getDuration());
+        }
+
+        public int getDuration() {
+            return (int)mMediaPlayer.getLength();
+        }
+
+        public boolean isPlaying() {
+            return mMediaPlayer.isPlaying();
+        }
+
+        public void pause() {
+            mMediaPlayer.pause();
+        }
+
+        public void seekTo(int pos) {
+            mMediaPlayer.setPosition((float)pos / getDuration());
+        }
+
+        public void start() {
+            mMediaPlayer.play();
+        }
+
+        public boolean canPause() {
+            return true;
+        }
+
+        public boolean canSeekBackward() {
+            return true;
+        }
+
+        public boolean canSeekForward() {
+            return true;
+        }
+
+        @Override
+        public int getAudioSessionId() {
+            return 0;
+        }
+    };
 }
