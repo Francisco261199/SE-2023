769b81f708d3c00f49a574b5f179467637e521d9
DaniilLystopadskyi <danlys1967@gmail.com>
2023-05-27 19:04:10 +0100

    Changed video player to support h264

--- src/Android/Ding/app/src/main/java/com/se/ding/VideoPlayerActivity.java
+++ src/Android/Ding/app/src/main/java/com/se/ding/VideoPlayerActivity.java
@@ -7,7 +7,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.MediaController;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.SimpleExoPlayer;
@@ -19,85 +26,55 @@ import com.google.android.exoplayer2.util.Util;
 import com.se.ding.network.Client;
 import com.se.ding.network.Stream;
 
+import org.videolan.libvlc.LibVLC;
+import org.videolan.libvlc.Media;
+import org.videolan.libvlc.MediaPlayer;
+
+import java.util.ArrayList;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-
-public class VideoPlayerActivity extends AppCompatActivity {
-    private PlayerView playerView;
-    private SimpleExoPlayer player;
-    private Boolean isStreaming = false;
+public class VideoPlayerActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private LibVLC libVLC;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
-        isStreaming = getIntent().getExtras().getBoolean("isStreaming");
 
-        playerView = findViewById(R.id.player_view);
+        surfaceView = findViewById(R.id.surface_view);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+
+        libVLC = new LibVLC(this);
+        mediaPlayer = new MediaPlayer(libVLC);
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        initializePlayer();
+    public void surfaceCreated(SurfaceHolder holder) {
+        mediaPlayer.getVLCVout().setVideoSurface(holder.getSurface(), null);
+        mediaPlayer.getVLCVout().attachViews();
+
+        Uri uri = Uri.parse(getIntent().getStringExtra("videoUrl"));
+        Media media = new Media(libVLC, uri);
+        mediaPlayer.setMedia(media);
+        media.release();
+
+        mediaPlayer.play();
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        releasePlayer();
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        releasePlayer();
-        if(isStreaming) {
-            SharedPreferences sharedPref = getSharedPreferences("preferences", Context.MODE_PRIVATE);
-            String accessToken = sharedPref.getString("access_token", null);
-            if (accessToken != null) {
-                Call<Void> call = Client.getService().stopStream("Bearer " + accessToken);
-                call.enqueue(new Callback<Void>() {
-                    @Override
-                    public void onResponse(Call<Void> call, Response<Void> response) {
-                    }
-
-                    @Override
-                    public void onFailure(Call<Void> call, Throwable t) {
-                    }
-                });
-            }
-        }
-    }
-
-    private void initializePlayer() {
-        // Create a new ExoPlayer instance
-        player = new SimpleExoPlayer.Builder(this).build();
-        Toast.makeText(this, "Player Initialized", Toast.LENGTH_SHORT).show();
-
-        // Attach the player to the player view
-        playerView.setPlayer(player);
-
-        // Create a media source pointing to the video URL
-        Uri videoUri = Uri.parse(getIntent().getStringExtra("videoUrl"));
-        Toast.makeText(this, "Video URL: " + videoUri, Toast.LENGTH_SHORT).show();
-        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
-                Util.getUserAgent(this, getString(R.string.app_name)));
-        MediaSource mediaSource = new ProgressiveMediaSource.Factory(dataSourceFactory)
-                .createMediaSource(videoUri);
-
-        // Prepare the player with the media source
-        player.setMediaSource(mediaSource);
-        player.prepare();
-        player.setPlayWhenReady(true);
-    }
-
-    private void releasePlayer() {
-        Toast.makeText(this, "Releasing player...", Toast.LENGTH_SHORT).show();
-        if (player != null) {
-            player.release();
-            player = null;
-        }
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        libVLC.release();
     }
 }
